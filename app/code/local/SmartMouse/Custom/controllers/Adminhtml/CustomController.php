184a209dd67087576b5b5aea93587b8e855c5e47
Smart Mouse <smart9mouse@gmail.com>
2019-10-12 00:52:03 +0300

    add save action

--- app/code/local/SmartMouse/Custom/controllers/Adminhtml/CustomController.php
+++ app/code/local/SmartMouse/Custom/controllers/Adminhtml/CustomController.php
@@ -45,4 +45,71 @@ class SmartMouse_Custom_Adminhtml_CustomController
     {
         $this->_forward('edit');
     }
+
+    public function saveAction()
+    {
+        $redirectBack = $this->getRequest()->getParam('back', false);
+        if ($data = $this->getRequest()->getPost()) {
+            if (isset($_FILES['image']['name']) && $_FILES['image']['name'] != '') {
+                $imageDir = Mage::helper('custom')->getImageDir();
+                try {
+                    $uploader = new Varien_File_Uploader('image');
+                    $uploader->setAllowedExtensions(array('jpeg', 'jpg', 'gif', 'png'));
+                    $uploader->setAllowRenameFiles(false);
+                    $uploader->setFilesDispersion(false);
+
+                    $path = 'media' . DS . $imageDir;
+                    $uploader->save($path, $_FILES['image']['name']);
+                } catch (Exception $e) {
+                    Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                }
+
+                $data['image'] = $imageDir . DS . $_FILES['image']['name'];
+            }
+
+            if(isset($data['image']['value']) && $data['image']['value'] != '') {
+                $data['image'] = $data['image']['value'];
+            }
+
+            $id = $this->getRequest()->getParam('id');
+            $model = Mage::getModel('custom/custom');
+            if ($id) {
+                $model->load($id);
+                if (!$model->getId()) {
+                    $this->_getSession()->addError(
+                        $this->__('This Game no longer exists.')
+                    );
+                    $this->_redirect('*/*/index');
+                    return;
+                }
+            }
+
+            try {
+                $model->addData($data);
+                $this->_getSession()->setFormData($data);
+                $model->save();
+                $this->_getSession()->setFormData(false);
+                $this->_getSession()->addSuccess(
+                    $this->__('The Game has been saved.')
+                );
+            } catch (Mage_Core_Exception $e) {
+                $this->_getSession()->addError($e->getMessage());
+                $redirectBack = true;
+            } catch (Exception $e) {
+                $this->_getSession()->addError(
+
+                    $this->__('Unable to save the Game.')
+
+                );
+                $redirectBack = true;
+                Mage::logException($e);
+            }
+
+            if ($redirectBack) {
+                $this->_redirect('*/*/edit', array('game_id' => $model->getId()));
+                return;
+            }
+        }
+        $this->_redirect('*/*/index');
+    }
 }
