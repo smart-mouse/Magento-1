446daa435b04d66121b196dc2bc7deb6131525ed
Smart Mouse <smart9mouse@gmail.com>
2019-10-12 00:16:55 +0300

    add edit action

--- app/code/local/SmartMouse/Custom/controllers/Adminhtml/CustomController.php
+++ app/code/local/SmartMouse/Custom/controllers/Adminhtml/CustomController.php
@@ -12,4 +12,32 @@ class SmartMouse_Custom_Adminhtml_CustomController
         $this->_title($this->__('Custom'));
         $this->renderLayout();
     }
+
+    public function editAction()
+    {
+        $id = $this->getRequest()->getParam('game_id');
+        $model = Mage::getModel('custom/custom');
+
+        if ($id) {
+            $model->load($id);
+            if (!$model->getId()) {
+                $this->_getSession()->addError(
+                    $this->__('This Game no longer exists.')
+                );
+                $this->_redirect('*/*/');
+                return;
+            }
+        }
+
+        $data = $this->_getSession()->getFormData(true);
+        if (!empty($data)) {
+            $model->setData($data);
+        }
+
+        Mage::register('current_model', $model);
+
+        $this
+            ->loadLayout()
+            ->renderLayout();
+    }
 }
