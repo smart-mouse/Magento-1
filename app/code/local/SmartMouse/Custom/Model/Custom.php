b5ef256bb0806a364bebeb9aecac21c43649fdcb
Smart Mouse <smart9mouse@gmail.com>
2019-10-11 22:27:48 +0300

    add options to select

--- app/code/local/SmartMouse/Custom/Model/Custom.php
+++ app/code/local/SmartMouse/Custom/Model/Custom.php
@@ -5,4 +5,13 @@ class SmartMouse_Custom_Model_Custom extends Mage_Core_Model_Abstract
     {
         $this->_init('custom/custom');
     }
+
+    public function toOptionArray()
+    {
+        return array(
+            '0' => Mage::helper('custom')->__('Item 0'),
+            '1' => Mage::helper('custom')->__('Item 1'),
+            '2' => Mage::helper('custom')->__('Item 2')
+        );
+    }
 }
